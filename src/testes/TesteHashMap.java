fc305999f26c9fbbcca6391a13a1250d9f924e28
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-05-29 19:26:04 -0300

    adicionado "prints" para ás informações

--- src/testes/TesteHashMap.java
+++ src/testes/TesteHashMap.java
@@ -14,22 +14,34 @@ import produto.Produto;
 public class TesteHashMap {
 
 	public static void main(String[] args) {
+		
+		System.out.println("-TEST- (HASHMAP)");
+		//Lista de produto (banco dados)
 		HashMap<String, Produto> listaProduto = new HashMap<String, Produto>();
 		listaProduto.put("F35",new Produto("F22","Macarrao",35.3f,new String[] {"TEST1","Test2"}));
 		listaProduto.put("F40",new Produto("F40","Test",25.5f,new String[] {"TEST1","Test2"}));
-		
 		listaProduto.get("F40").setCategorias(new String[]{"Automovel","CARRO"});
 		listaProduto.get("F40").removeCategoria(1);
 		
-		System.out.println(listaProduto.get("F35").getLocalProduzido());
+		System.out.println("\n\n-PRINT- BANCO-DADOS");
+		for (String chaves : listaProduto.keySet()) {
+
+            //Capturamos o valor a partir da chave
+            String nome = listaProduto.get(chaves).getNome();
+            String categorias="";
+            
+            for(String i:listaProduto.get(chaves).getCategoriasToArray()) {
+            	categorias+=i+",";
+            }
+            System.out.println(chaves + " = " + nome + "	:["+categorias+"]");
+		}
 		
+		//Lista do usuário (compra)
 		HashMap<String, CompraProduto> carrinho = new HashMap<String, CompraProduto>();
 		carrinho.put("F23", new CompraProduto(listaProduto.get("F40"),5));
+
 		
-		//carrinho.get("F23").setGarantia(new GarantiaExtendida("básica", 6, 750.0f, "Nenhuma"));
-		//carrinho.get("F23").setPrecoTotal();
-		
-		System.out.println("-TEST- 3");
+		System.out.println("\n\n-PRINT- COMPRAS");
 		for (String chaves : carrinho.keySet()) {
 
             //Capturamos o valor a partir da chave
